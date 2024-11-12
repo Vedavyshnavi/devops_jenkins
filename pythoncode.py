bf819cc9fec08341b2b13bb9abb503df251b3ea2
Vedavyshnavi <140415633+Vedavyshnavi@users.noreply.github.com>
2024-11-12 12:15:45 +0530

    Add files via upload

---
+++ pythoncode.py
@@ -0,0 +1,15 @@
+   pipeline {
+  agent any
+  stages {
+    stage('version') {
+      steps {
+        bat 'python --version' // Checks Python version
+      }
+    }
+    stage('hello') {
+      steps {
+        bat 'C:\\Users\\hp\\AppData\\Local\\Programs\\Python\\Python311\\python.exe p1.py' 
+      }
+    }
+  }
+}
